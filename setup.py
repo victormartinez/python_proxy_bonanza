38d1cc89bacf0df11cf1fcc22f7fe26a1f066844
Victor Martinez <vcrmartinez@gmail.com>
2016-09-29 01:41:36 -0300

    Change version to 1.0.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ tests_require = _to_list(parse_requirements('requirements-test.txt', session=Fal
 
 setup(
     name='proxy_bonanza',
-    version='1.0.0',
+    version='1.0.1',
     description='A lightweight python client for Proxy Bonanza service',
     long_description=long_description,
     url='http://github.com/victormartinez/python_proxy_bonanza',
